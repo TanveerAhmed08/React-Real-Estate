194bc1f0a3d2bb3839a7bf75623c07a7c0659612
Tanveer Ahmed <tanveerahmed08.1999@gmail.com>
2024-12-03 21:54:14 +0500

    New Work

---
+++ src/components/pages/Pages.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../common/header/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../home/Home";
+const Pages = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+        <Route exact path="/" Component={Home} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default Pages;
